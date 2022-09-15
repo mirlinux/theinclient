9c848dcb28aaa4ec17293cf8152f17e24ef4d328
DESKTOP-DNBU2VU\oem <iamjiyoo@gmail.com>
2022-09-15 14:52:25 +0900

    Project 구성 수정

---
+++ ClientApp/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace ClientApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbUserId = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(85, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "아이디";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(73, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "비밀번호";
+            // 
+            // tbUserId
+            // 
+            this.tbUserId.Location = new System.Drawing.Point(151, 48);
+            this.tbUserId.Name = "tbUserId";
+            this.tbUserId.PlaceholderText = "아이디 입력해";
+            this.tbUserId.Size = new System.Drawing.Size(100, 23);
+            this.tbUserId.TabIndex = 2;
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(151, 79);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.PasswordChar = '*';
+            this.tbPassword.PlaceholderText = "비밀번호 입력해";
+            this.tbPassword.Size = new System.Drawing.Size(100, 23);
+            this.tbPassword.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Navy;
+            this.button1.Font = new System.Drawing.Font("휴먼매직체", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(73, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 32);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "로그인";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.button2.Font = new System.Drawing.Font("휴먼매직체", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(74, 156);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(177, 32);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "회원가입";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 235);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbUserId);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "로그인";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox tbUserId;
+        private TextBox tbPassword;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ ClientApp/config.cs
@@ -0,0 +1,10 @@
+namespace ClientApp
+{
+    static class Config
+    {
+        static public string DB_SERVER      = "192.168.0.8";
+        static public string DB_USERID      = "student";
+        static public string DB_PASSWORD    = "studentpass";
+        static public string DB_DATABASE    = "thein";
+    }
+}
